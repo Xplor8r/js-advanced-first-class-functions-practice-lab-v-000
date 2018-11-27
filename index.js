54b0e2f1a068fbcbf7ccf4d5a12dd34f0e19e91b
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 02:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // Code your solution in this file!
-const logDriverNames = function(array) {
-  array.forEach( function(d) { console.log(d.name); });
+const logDriverNames = function(arr) {
+  arr.forEach( function(d) { console.log(d.name); });
 };
-const logDriversByHometown = function(array, arg) {
-  array.forEach( function(d) { if (d.hometown === arg) console.log(d.name); });
-};
-
-const driversByRevenue = function(array) {
-  return array.slice().sort( function(a, b) { return a.revenue - b.revenue; });
+const logDriversByHometown = function(arr, arg) {
+  arr.forEach( function(d) { if (d.hometown === arg) console.log(d.name); });
 };
 
-const driversByName = function(array) {
-  return array.slice().sort( function(a, b) { return a.name.localeCompare(b.name); });
+const driversByRevenue = function(arr) {
+  return arr.slice().sort( function(a, b) { return a.revenue - b.revenue; });
 };
 
-const totalRevenue = function(array) {
-  return array.reduce( function(tot, d) { return d.revenue + tot; }, 0);
+const driversByName = function(arr) {
+  return arr.slice().sort( function(a, b) { return a.name.localeCompare(b.name); });
 };
 
-const averageRevenue = function(array) {
-  return totalRevenue(array) / array.length
+const totalRevenue = function(arr) {
+  return arr.reduce( function(tot, d) { return d.revenue + tot; }, 0);
+};
+
+const averageRevenue = function(arr) {
+  return totalRevenue(arr) / arr.length
 };
