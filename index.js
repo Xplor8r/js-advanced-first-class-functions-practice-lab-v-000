120b46dd3dd771aba0c9c0d213eaeb0ba59e8cd0
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 02:25:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const driversByName = function(array) {
 };
 
 const totalRevenue = function(array) {
-  return array.reduce(function(total, driver) { return driver.revenue + total; }, 0);
+  return array.reduce(function(total, d) { return d.revenue + total; }, 0);
 };
 
 const averageRevenue = function(array) {
