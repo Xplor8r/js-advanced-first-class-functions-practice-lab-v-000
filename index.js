4de5369605dfc7d4019f094395d53082f0e51256
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 02:05:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Code your solution in this file!
 const logDriverNames = function(array) {
-  array.forEach(function(driver) { console.log(driver.name); });
+  array.forEach( function(driver) { console.log(driver.name); });
 }
-const logDriversByHometown = function (array, arg) {
-  array.forEach(function(driver) { if (driver.hometown === arg) console.log(driver.name); });
+const logDriversByHometown = function(array, arg) {
+  array.forEach( function(driver) { if (driver.hometown === arg) console.log(driver.name); });
+}
+
+const driversByRevenue = function(array) {
+  return array.sort( function(a, b) { return a.revenue - b.revenue; });
 }
