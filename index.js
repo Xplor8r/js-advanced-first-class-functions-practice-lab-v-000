26ee42d826c8e2bf9bc6585ed2fa563703c880a1
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 02:24:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const logDriverNames = function(array) {
   array.forEach( function(d) { console.log(d.name); });
 };
 const logDriversByHometown = function(array, arg) {
-  array.forEach( function(driver) { if (driver.hometown === arg) console.log(driver.name); });
+  array.forEach( function(d) { if (d.hometown === arg) console.log(d.name); });
 };
 
 const driversByRevenue = function(array) {
