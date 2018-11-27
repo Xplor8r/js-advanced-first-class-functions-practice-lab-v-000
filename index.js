23bf63eab57d4cc1da4bdf74de02e7dee952f229
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-27 02:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file!
 const logDriverNames = function(array) {
-  array.forEach(function (driver) { console.log(driver.name); });
+  array.forEach(function(driver) { console.log(driver.name); });
+}
+const logDriversByHometown = function (array, arg) {
+  array.forEach(function(driver) { if (driver.hometown === arg) console.log(driver.name); });
 }
